3f69fc1c2cafea0710cf817aafe0dd3a29a28bfd
magistermilitum <37591896+magistermilitum@users.noreply.github.com>
2022-02-06 17:42:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,7 +129,31 @@ def read_image(image_name):
   return img
 
 
+@st.cache()
+def segmentation(image):
+  model_path = 'models/blla.mlmodel'
+  model = vgsl.TorchVGSLModel.load_model(model_path)
+  #segmentación de la imagen
+  baseline_seg = blla.segment(image, model=model)
+  print("final de segmentación")
   
+  return baseline_seg
+  
+  
+@st.cache()
+def transcript(img, baseline_seg):
+  #aplicación del modelo de reconocimiento
+  rec_model_path = 'models/model_36.mlmodel'
+  modelito = models.load_any(rec_model_path)
+  
+  pred_it = rpred.rpred(network=modelito, im=img, bounds=baseline_seg)
+  #obtención de las predicciones
+  pred_char=[]
+  for record in pred_it:
+    #print(record)
+    pred_char.append(record.prediction)
+    
+  return " ".join(pred_char)
   
 
 #############
