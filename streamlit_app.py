c6f3d2f607a61c8761c1f86efa3c0f7379d514c8
magistermilitum <37591896+magistermilitum@users.noreply.github.com>
2022-02-06 18:19:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -262,7 +262,7 @@ if nav == 'Summarize text':
                     #image = file.read()
                     #resultado=transcript(read_image(file), segmentation(read_image(file)))
                     st.image(read_image(file),width=250)
-                    st.write(read_image(file).size)
+                    st.write(segmentation(read_image(file)))
                     st.success("mamita 3")
                     
 
